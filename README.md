7998f53df3878499ad4df071f36e99559b7f95c0
yves songolo <yves.songolo@gmail.com>
2019-02-08 10:46:54 -0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# Square-geofence-region
---
+++ Square geofence region/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Square geofence region
+//
+//  Created by Yves Songolo on 2/8/19.
+//  Copyright © 2019 Yves Songolo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
